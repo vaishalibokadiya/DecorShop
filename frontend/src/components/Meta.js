570a1a693598b32ce75f9ea7eb2f87c40139729a
vaishalibokadiya <vaishali.bokadiya@gmail.com>
2023-01-12 20:54:17 +0530

    First commit

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keyword" content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Welcome To DecorShop",
+  description: "We sell the best products for cheap",
+  keywords: "decor, buy decor, cheap decor",
+};
+
+export default Meta;
